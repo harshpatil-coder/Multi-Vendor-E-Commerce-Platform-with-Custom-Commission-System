17320d93fcf6eb10aa3932d8ef045f6682578aa2
saurav-coder155 <prajapatisaurav33@gmail.com>
2025-05-20 17:45:09 +0530

    login page done.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
       </div>
      
       <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a href="#" class="text-sm/6 font-semibold text-white">Log in <span aria-hidden="true">&rarr;</span></a>
+        <a href="/login" class="text-sm/6 font-semibold text-white">Log in <span aria-hidden="true">&rarr;</span></a>
       </div>
     </nav>
     <!-- Mobile menu, show/hide based on menu open state. -->
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SessionController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [SessionController::class, 'create'])->name('login');
+
